6fc98e9d2b851234956443d576ee8945ef55197c
muhammadjamshed <ajmehsud12@gmail.com>
2024-09-30 02:37:30 +0500

    Revert "cors added"
    This reverts commit a2a3c3c19aa4d9050523054a617501b84d5c2ff1.

--- ecom-backend/server.js
+++ ecom-backend/server.js
@@ -16,10 +16,7 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: '*', // Allow all origins, or specify the frontend domain like 'http://localhost:3000'
-  credentials: true, // If you're handling cookies or session-based authentication
-}));
+app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to MongoDB
